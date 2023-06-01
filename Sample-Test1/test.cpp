1e74ab1acf8e5e79cb1c64ef650ac9901ac4da73
kiho <kiho.kil@gmail.com>
2023-06-01 15:38:42 +0900

    [feature] p38

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -3,8 +3,21 @@
 
 using namespace std;
 
-TEST(TestCaseName, TestName) {
+class PrimeFixture : public testing::Test
+{
+public:
 	PrimeFactor prime_factor;
-	vector<int> expected = {  };
+	vector<int> expected;
+};
+
+TEST_F(PrimeFixture, Of1) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
+}
+
+TEST_F(PrimeFixture, Of2) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
 }
